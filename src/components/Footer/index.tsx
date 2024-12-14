0f1709d640412a2d447334d4d262d30cfc7e2e0c
Ahmad Abdelraheem <ahmadabdelraheem@outlook.com>
2024-12-14 03:03:09 +0300

    update footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
       </section>
 
       <section className={style.contactInfo}>
-        <p>Telephone: 123-456-789</p>
+        <p>phone number: 07 7587 8389</p>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="currentColor"
@@ -20,16 +20,7 @@ const Footer = () => {
       </section>
 
       <section className={style.copyrights}>
-      <p>All copyrights reserved</p>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="currentColor"
-          className="bi bi-c-square"
-          viewBox="0 0 16 16"
-        >
-          <path d="M8.146 4.992c-1.212 0-1.927.92-1.927 2.502v1.06c0 1.571.703 2.462 1.927 2.462.979 0 1.641-.586 1.729-1.418h1.295v.093c-.1 1.448-1.354 2.467-3.03 2.467-2.091 0-3.269-1.336-3.269-3.603V7.482c0-2.261 1.201-3.638 3.27-3.638 1.681 0 2.935 1.054 3.029 2.572v.088H9.875c-.088-.879-.768-1.512-1.729-1.512" />
-          <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm15 0a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1z" />
-        </svg>
+      <p>all copyrights reserved &copy;</p>
       </section>
       <section className={style.credits}>
         <p>powered by: <a href="https://www.linkedin.com/in/ahmad-abdelraheem-26a56630b/">Ahmad Abdalraheem</a>, <a href="https://www.linkedin.com/in/mohammed-aliz/">Mohammad Hanoun</a></p>
